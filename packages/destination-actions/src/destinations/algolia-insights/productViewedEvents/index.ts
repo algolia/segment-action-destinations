1883b32c1037df55cec86ecabb7de87574e872d1
Beatrice Parfait <beatrice.parfait@algolia.com>
2023-02-02 14:38:43 +1100

    remove default imports for actions

--- packages/destination-actions/src/destinations/algolia-insights/productViewedEvents/index.ts
+++ packages/destination-actions/src/destinations/algolia-insights/productViewedEvents/index.ts
@@ -4,47 +4,7 @@ import { AlgoliaBehaviourURL, AlgoliaProductViewedEvent } from '../algolia-insig
 import type { Settings } from '../generated-types'
 import type { Payload } from './generated-types'
 
-export const productViewedPresets: Subscription[] = [
-  // and is $ meant to represent something specific?
-  {
-    name: 'Product ID',
-    subscribe: 'type = "track" and event = "Product Viewed"',
-    partnerAction: 'properties',
-    mapping: defaultValues({ '@path': '$.properties.product_id' })
-  },
-  {
-    name: 'Index',
-    subscribe: 'type = "track" and event = "Product Viewed"',
-    partnerAction: 'properties',
-    mapping: defaultValues({ '@path': '$.properties.search_index' })
-  },
-  {
-    name: 'Query ID',
-    subscribe: 'type = "track" and event = "Product Viewed"',
-    partnerAction: 'properties',
-    mapping: defaultValues({ '@path': '$.properties.query_id' })
-  },
-  {
-    name: 'Anonymous ID',
-    subscribe: 'type = "track" and event = "Product Viewed"',
-    partnerAction: 'anonymousId',
-    mapping: defaultValues({ '@path': '$.anonymousId' })
-  },
-  {
-    name: 'User ID',
-    subscribe: 'type = "track" and event = "Product Viewed"',
-    partnerAction: 'userId',
-    mapping: defaultValues({ '@path': '$.userId' })
-  },
-  {
-    name: 'timestamp',
-    subscribe: 'type = "track" and event = "Product Viewed"',
-    partnerAction: 'timestamp',
-    mapping: defaultValues({ '@path': '$.timestamp' })
-  }
-]
-
-const action: ActionDefinition<Settings, Payload> = {
+export const productViewedEvents: ActionDefinition<Settings, Payload> = {
   title: 'Product Viewed Events',
   description: 'Product views which can be tied back to an Algolia Search, Recommend or Predict result',
   fields: {
@@ -77,15 +37,17 @@ const action: ActionDefinition<Settings, Payload> = {
     },
     anonymousID: {
       label: 'Anonymous ID',
-      description: "The user's anonymous id.",
+      description:
+        "The user's anonymous id. Optional if User ID is provided. See Segment [common fields documentation](https://segment.com/docs/connections/spec/common/)",
       type: 'string',
-      required: true,
+      required: false,
       default: { '@path': '$.anonymousId' }
     },
     userID: {
       type: 'string',
       required: false,
-      description: 'The ID associated with the user',
+      description:
+        'The ID associated with the user. Optional if Anonymous ID is provided. See Segment [common fields documentation](https://segment.com/docs/connections/spec/common/)',
       label: 'User ID',
       default: { '@path': '$.userId' }
     },
@@ -104,7 +66,7 @@ const action: ActionDefinition<Settings, Payload> = {
       eventName: 'Product Viewed',
       eventType: 'view',
       objectIDs: [data.payload.objectID],
-      userToken: data.payload.userID || data.payload.anonymousID,
+      userToken: (data.payload.userID || data.payload.anonymousID) as string,
       timestamp: data.payload.timestamp ? new Date(data.payload.timestamp).valueOf() : undefined
     }
     const insightPayload = { events: [insightEvent] }
@@ -116,4 +78,10 @@ const action: ActionDefinition<Settings, Payload> = {
   }
 }
 
-export default action
+/** used in the quick setup */
+export const productViewedPresets: Subscription = {
+  name: 'Send product viewed events to Algolia',
+  subscribe: productViewedEvents.defaultSubscription as string,
+  partnerAction: 'productViewedEvents',
+  mapping: defaultValues(productViewedEvents.fields)
+}
